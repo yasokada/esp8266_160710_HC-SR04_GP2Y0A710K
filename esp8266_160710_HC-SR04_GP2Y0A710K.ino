a501469463dfae2c7c479e427cf1caac5ad6297f
Yasuhiko OKADA <yasokada@gmail.com>
2016-07-10 12:44:26 +0900

    send data to udpLogger

--- esp8266_160710_HC-SR04_GP2Y0A710K.ino
+++ esp8266_160710_HC-SR04_GP2Y0A710K.ino
@@ -1,10 +1,16 @@
 #include <ESP8266WiFi.h>
+#include "esp8266_160602_udpTxToLogger.h"
 
 extern "C" {
 #include "user_interface.h"
 }
 
 /*
+ * v0.3 2016 Jul. 10
+ *   - send data to udpLogger
+ *     + modify loop()
+ *     + add sendToUdpLogger();
+ *     + use [esp8266_160602_udpTxToLogger]
  * v0.2 2016 Jul. 10
  *   - add ADC for GP2Y0A710K
  * v0.1 2016 Jul. 09
@@ -18,13 +24,34 @@ static const int kTrigPin = 12;
 static const int kEchoPin = 13;
 
 void setup() {
-  WiFi.disconnect(); // avoid watchdog reset by WiFi thread
-
   Serial.begin(115200);
   Serial.println("");
 
   pinMode(kTrigPin, OUTPUT);
   pinMode(kEchoPin, INPUT);
+
+  // UDP
+  WiFi_setup();
+  WiFi_printConnectionInfo();
+}
+
+void sendToUdpLogger(float dst_cm, float voltage)
+{
+  char szbuf[100];
+  int pos = 0;
+  int whl, frac; // whole and fractional parts
+
+  // 1. distance
+  whl = (int)dst_cm;
+  frac = (int)(dst_cm * 100) % 100;
+  pos = sprintf(&szbuf[pos],",dst(cm)=,%d.%02d", whl, frac);
+
+  // 2. voltage
+  whl = (int)voltage;
+  frac = (int)(voltage * 100) % 100;
+  pos = sprintf(&szbuf[pos],",voltage(v)=,%d.%02d\r\n", whl, frac);
+ 
+  WiFi_txMessage(szbuf);    
 }
 
 void loop() {
@@ -51,6 +78,8 @@ void loop() {
   Serial.print("\t");
   Serial.print(voltage);
   Serial.println();
+
+  sendToUdpLogger(dst_cm, voltage);
     
   delay(300); // msec
 
